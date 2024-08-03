0670c91ff5cb7214aef0eefa6b4322e1dfb21f99
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-03 12:43:12 +0200

    contact fields, getters and setters

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 11:57:15 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/03 12:00:47 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/03 12:42:52 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,3 +19,15 @@ Contact::Contact() {
 Contact::~Contact() {
 	
 }
+
+std::string Contact::getName()          {return this->name;}
+std::string Contact::getSurname()       {return this->surname;}
+std::string Contact::getNickname()      {return this->nickname;}
+std::string Contact::getPhoneNumber()   {return this->phoneNumber;}
+std::string Contact::getDarkestSecret() {return this->darkestSecret;}
+
+void Contact::setName(std::string name)                   {this->name = name;}
+void Contact::setSurname(std::string surname)             {this->surname = surname;}
+void Contact::setNickname(std::string nickname)           {this->nickname = nickname;}
+void Contact::setPhoneNumber(std::string phoneNumber)     {this->phoneNumber = phoneNumber;}
+void Contact::setDarkestSecret(std::string darkestSecret) {this->darkestSecret = darkestSecret;}
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -6,19 +6,37 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 11:57:17 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/03 12:00:54 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/03 12:39:36 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
+#include <string>
+
 class Contact {
 private:
-
+	std::string name;
+	std::string surname;
+	std::string nickname;
+	std::string phoneNumber;
+	std::string darkestSecret;
 public:
 	Contact();
 	~Contact();
+
+	std::string getName();
+	std::string getSurname();
+	std::string getNickname();
+	std::string getPhoneNumber();
+	std::string getDarkestSecret();
+	
+	void setName(std::string name);
+	void setSurname(std::string surname);
+	void setNickname(std::string nickname);
+	void setPhoneNumber(std::string phoneNumber);
+	void setDarkestSecret(std::string darkestSecret);
 };
 
 #endif
