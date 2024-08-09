72abb5554b6ad77065866deedd0416c016a7c94d
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-09 19:14:25 +0200

    cpp01 ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 19:33:26 by tunsal            #+#    #+#             */
+/*   Updated: 2024/08/09 19:14:12 by tunsal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <string>
+
+int main(int argc, char *argv[]) {
+	// Get 3 arguments <filename>, <targetStr>, <replacementStr>
+	if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <filename> <target string> <replacement string>" << std::endl;
+        return 0;
+    }
+	
+	std::string filename = argv[1];
+	std::string targetStr = argv[2];
+	std::string replacementStr = argv[3];
+	
+	// Open file <filename> (FORBIDDEN: C file manipulation functions)
+	std::ifstream ifs(filename.c_str());
+	if (!ifs) {
+		std::cerr << "Error: Could not open file " << filename << std::endl;
+        return EXIT_FAILURE;
+	}
+	
+	// Replace all strings <s1> with <s2> (FORBIDDEN: std::string::replace())
+	std::stringstream strStream;
+	strStream << ifs.rdbuf();
+	ifs.close();
+	std::string buffer = strStream.str();
+
+	size_t i = 0;
+	i = buffer.find(targetStr, i);   // look for a match
+	while (i != std::string::npos) { // if match is found
+		// Found a match, handle
+		buffer.erase(i, targetStr.length());
+		buffer.insert(i,replacementStr);
+		i += replacementStr.length();
+		
+		// Look for the next match
+		i = buffer.find(targetStr, i);
+	}
+	
+	// Save file <filename>	(FORBIDDEN: C file manipulation functions)
+	std::ofstream ofs(filename.c_str());
+	if (!ofs) {
+        std::cerr << "Error: Could not create file " << filename << std::endl;
+        return EXIT_FAILURE;
+    }
+
+	ofs << buffer;
+	ofs.close();
+}
