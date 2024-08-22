4190dc98952e1e1a9c7001a2918be7367a5999b6
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-22 15:31:46 +0200

    ScavTrap version of old ClapTrap tests

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -12,18 +12,70 @@
 
 #include <iostream>
 #include <string>
+#include <limits>
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
-// static void printHeader(std::string header) {
-// 	std::cout                                                   << std::endl;
-// 	std::cout << "============================================" << std::endl;
-// 	std::cout << header                                         << std::endl;
-// 	std::cout << "============================================" << std::endl;
-// }
+static void printHeader(std::string header) {
+	std::cout                                                   << std::endl;
+	std::cout << "============================================" << std::endl;
+	std::cout << header                                         << std::endl;
+	std::cout << "============================================" << std::endl;
+}
+
+static void legacyTests() {
+	printHeader("Constructors, assignment, destructor");
+	{
+		ScavTrap n = ScavTrap();
+		ScavTrap j = ScavTrap("Joe");
+		
+		ScavTrap j_cpy = ScavTrap(j);
+		std::cout << "j_cpy name  = " << j_cpy.getName() << std::endl;
+		
+		ScavTrap j_cpy2 = j_cpy; // calls copy constructor too due to temporary compiler object
+		std::cout << "j_cpy2 name = " << j_cpy2.getName() << std::endl;
+	}
+
+	printHeader("Attack, takeDamage, beRepaired, underflow, overflow");
+	{
+		ScavTrap felon = ScavTrap("Felon");
+		
+		felon.attack("victim");
+		felon.takeDamage(2);
+		felon.beRepaired(1);
+	}
+
+	printHeader("underflow, overflow");
+	{
+		ScavTrap victim = ScavTrap("Victim");
+		
+		victim.beRepaired(std::numeric_limits<unsigned int>::max());
+		victim.beRepaired(std::numeric_limits<unsigned int>::max());
+		
+		victim.takeDamage(1000);
+		victim.takeDamage(std::numeric_limits<unsigned int>::max());
+	}
+
+	printHeader("Insufficent energy");
+	{
+		ScavTrap tired = ScavTrap("Tired");
+
+		for (int i = 0; i < ST_DEFAULT_ENERGYPOINTS + 1; ++i) {
+			tired.attack("victim");
+		}
+	}
+	
+	printHeader("Insufficient hitpoints");
+	{
+		ScavTrap dead = ScavTrap("Dead");
+
+		dead.takeDamage(ST_DEFAULT_HITPOINTS);
+		dead.attack("Chicken");
+	}
+}
 
 int main() {
-	
+	legacyTests();
 
 	return 0;
 }
