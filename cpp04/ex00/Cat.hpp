5f3a399efcfd0e18135541778b33ecf0f684020e
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-24 03:44:26 +0200

    cat and dog inherits

---
+++ cpp04/ex00/Cat.hpp
@@ -0,0 +1,10 @@
+#ifndef CAT_H
+#define CAT_H
+
+#include "Animal.hpp"
+
+class Cat : public Animal {
+
+};
+
+#endif
---
+++ cpp04/ex00/Dog.hpp
@@ -0,0 +1,10 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include "Animal.hpp"
+
+class Dog : public Animal {
+
+};
+
+#endif
