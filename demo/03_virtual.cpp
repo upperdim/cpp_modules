2f65d625d205cd7777d96f979da984d1176f198a
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-25 23:29:14 +0200

    simplify class and add test combinations to main

--- demo/03_virtual.cpp
+++ demo/03_virtual.cpp
@@ -1,66 +1,67 @@
 #include <iostream>
 #include <string>
 
+static void printHeader(std::string header) {
+	std::cout                                                   << std::endl;
+	std::cout << "============================================" << std::endl;
+	std::cout << header                                         << std::endl;
+	std::cout << "============================================" << std::endl;
+}
+
 class A {
-	protected:
-		std::string _name;
 	public:
-		A()                   { this->_name = "A"; }
-		std::string getName() { return this->_name; }
-		std::string talk()    { std::cout << "A talks" << std::endl; }
+		A()         { std::cout << "A default constructor" << std::endl; }
+		~A()        { std::cout << "A destroyed" << std::endl; }
+		void talk() { std::cout << "A talks" << std::endl; }
 };
 
 class B : public A {
-	private:
 	public:
-		B()                   { this->_name = "B"; }
-		std::string getName() { return this->_name; }
-		std::string talk()    { std::cout << "B talks" << std::endl; }
-};
-
-// Base class
-class Base {
-public:
-    // Virtual function that can be overridden
-    void showMessage() {
-        std::cout << "Message from Base class" << std::endl;
-    }
-};
-
-// Derived class
-class Derived : public Base {
-public:
-    // Override the base class function
-    void showMessage() {
-        std::cout << "Message from Derived class" << std::endl;
-    }
+		B()         { std::cout << "B default constructor" << std::endl; }
+		~B()        { std::cout << "B destroyed" << std::endl; }
+		void talk() { std::cout << "B talks" << std::endl; }
 };
 
 int main() {
-	A a = A();
-	B b = B();
+	printHeader("Stack default constructor");
+	{
+		A a;
+		B b;
 
-	std::cout << a.getName() << std::endl;
-	std::cout << b.getName() << std::endl;
-	a.talk();
-	b.talk();
+		a.talk();
+		b.talk();
+	}
 
-	// A *pa = new A();
-	// B *pb = new B();
-	
-	// std::cout << a.getName() << std::endl;
-	// std::cout << b.getName() << std::endl;
-	// a.talk();
-	// b.talk();
+	printHeader("Stack default constructor (explicit)");
+	{
+		A a = A();
+		B b = B();
 
-	// delete pa;
-	// delete pb;
+		a.talk();
+		b.talk();
+	}
 
-	Base baseObj;
-    Base* ptr = &baseObj;
-    ptr->showMessage();
+	printHeader("Stack pointer");
+	{
+		A a;
+		B b;
+		
+		A *pa = &a;
+		B *pb = &b;
 
-    Derived derivedObj;
-	Derived* d_ptr = &derivedObj;    
-    d_ptr->showMessage();
+		pa->talk();
+		pb->talk();
+	}
+
+	printHeader("Heap pointer");
+	{
+		A *pa = new A();
+		B *pb = new B();
+
+		pa->talk();
+		pb->talk();
+
+		delete pb;
+		delete pa;
+	}
 }
