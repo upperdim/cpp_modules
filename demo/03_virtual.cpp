4ee041f0a07fff962421622bfc4617bf185827b8
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-25 23:42:09 +0200

    added examples for virtual

--- demo/03_virtual.cpp
+++ demo/03_virtual.cpp
@@ -10,16 +10,18 @@ static void printHeader(std::string header) {
 
 class A {
 	public:
-		A()         { std::cout << "A default constructor" << std::endl; }
-		~A()        { std::cout << "A destroyed" << std::endl; }
-		void talk() { std::cout << "A talks" << std::endl; }
+		A()                  { std::cout << "A default constructor" << std::endl; }
+		virtual ~A()         { std::cout << "A destroyed" << std::endl; }
+		void talk()          { std::cout << "A talks" << std::endl; }
+		virtual void vtalk() { std::cout << "A vtalks" << std::endl; }
 };
 
 class B : public A {
 	public:
-		B()         { std::cout << "B default constructor" << std::endl; }
-		~B()        { std::cout << "B destroyed" << std::endl; }
-		void talk() { std::cout << "B talks" << std::endl; }
+		B()                  { std::cout << "B default constructor" << std::endl; }
+		~B()                 { std::cout << "B destroyed" << std::endl; }
+		void talk()          { std::cout << "B talks" << std::endl; }
+		virtual void vtalk() { std::cout << "B vtalks" << std::endl; }
 };
 
 int main() {
@@ -62,6 +64,19 @@ int main() {
 		pb->talk();
 
 		delete pb;
+		delete pa;
+	}
+
+	printHeader("Needs virtual");
+	{
+		// Polymorphism gets involved with base class pointer to derived object
+		A *pa = new B();
+
+		pa->talk();
+		pa->vtalk();
+
+		// A destructor also needs to be virtual
+		// otherwise A is destroyed but B is not, can create undefined behaviour
 		delete pa;
 	}
 }
