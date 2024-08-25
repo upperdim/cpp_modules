249fdcdd7bbc7e305c24d99699f09f54ce4c5ae9
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-25 22:20:48 +0200

    easier tracking

--- demo/02_allocs_constructors.cpp
+++ demo/02_allocs_constructors.cpp
@@ -26,16 +26,21 @@ class Demo {
 
 int main() {
 	Demo d1;
+	std::cout << "d1 name = " << d1.getName() << std::endl;
 
 	Demo d2 = Demo();
+	std::cout << "d2 name = " << d2.getName() << std::endl;
 
 	Demo d3 = Demo("d3");
+	std::cout << "d3 name = " << d3.getName() << std::endl;
 
 	Demo *p_d4 = new Demo("d4");
+	std::cout << "*p_d4 name = " << p_d4->getName() << std::endl;
+	delete p_d4;
 
 	Demo d5;
+	std::cout << "d5 name = " << d5.getName() << std::endl;
 	Demo *p_d5 = &d5;
+	std::cout << "*p_d5 name = " << p_d5->getName() << std::endl;
 	delete p_d5;
-
-	delete p_d4;
 }
