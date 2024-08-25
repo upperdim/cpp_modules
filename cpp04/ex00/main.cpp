9e457da1dd0b1232df31f43cf50e2ecb9458b225
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-25 03:09:26 +0200

    subject tests

---
+++ cpp04/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tunsal <tunsal@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 02:57:40 by tunsal            #+#    #+#             */
+/*   Updated: 2024/08/25 02:57:40 by tunsal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+
+int main() {
+	const Animal* meta = new Animal();
+	const Animal*  dog = new Dog();
+	const Animal*  cat = new Cat();
+
+	std::cout << dog->getType() << " " << std::endl;
+	std::cout << cat->getType() << " " << std::endl;
+
+	cat->makeSound(); //will output the cat sound!
+	dog->makeSound();
+	meta->makeSound();
+
+	return 0;
+}
