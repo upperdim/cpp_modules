50f378f3cf85beeedfd06035d13926ec4b341e3f
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-25 23:44:52 +0200

    descriptions of allocations

--- demo/02_allocs_constructors.cpp
+++ demo/02_allocs_constructors.cpp
@@ -25,19 +25,24 @@ class Demo {
 };
 
 int main() {
+	// Stack default constructor
 	Demo d1;
 	std::cout << "d1 name = " << d1.getName() << std::endl;
 
+	// Stack default constructor (explicit)
 	Demo d2 = Demo();
 	std::cout << "d2 name = " << d2.getName() << std::endl;
 
+	// Stack name constructor
 	Demo d3 = Demo("d3");
 	std::cout << "d3 name = " << d3.getName() << std::endl;
 
+	// Heap name constructor
 	Demo *p_d4 = new Demo("d4");
 	std::cout << "*p_d4 name = " << p_d4->getName() << std::endl;
 	delete p_d4;
 
+	// Stack pointer
 	Demo d5;
 	std::cout << "d5 name = " << d5.getName() << std::endl;
 	Demo *p_d5 = &d5;
