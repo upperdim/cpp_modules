6695ce36f4b1ae1f887bc0e03a723cb680292cbd
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-29 01:26:23 +0200

    fixed uninitialized access segfault

--- cpp04/ex01/Cat.cpp
+++ cpp04/ex01/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 13:54:09 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/29 00:36:28 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/29 01:25:30 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ Cat::Cat() : Animal() {
 
 Cat::Cat(const Cat & from) : Animal() {
 	std::cout << "Cat copy constructor called" << std::endl;
+	this->_type = "Cat";
+	this->_brain = new Brain();
 	*this = from; // calls = operator overload
 }
 
@@ -32,7 +34,7 @@ Cat::~Cat() {
 Cat &Cat::operator=(Cat const & rhs) {
 	if (this != &rhs) {
 		this->_type = rhs._type;
-		this->_brain = rhs._brain;
+		*(this->_brain) = *(rhs._brain);
 	}
 	return *this;
 }
