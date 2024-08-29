d24db67579d7ff7363d0c13f0995132bb5f323eb
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-29 03:53:01 +0200

    added given header and removed cpp since it's an interface

--- cpp04/ex03/ICharacter.hpp
+++ cpp04/ex03/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 03:16:05 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/29 03:16:47 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/29 03:52:21 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 #define ICHARACTER_H
 
 class ICharacter {
-
+public:
+	virtual ~ICharacter() {}
+	virtual std::string const & getName() const = 0;
+	virtual void equip(AMateria* m) = 0;
+	virtual void unequip(int idx) = 0;
+	virtual void use(int idx, ICharacter& target) = 0;
 };
 
 #endif
