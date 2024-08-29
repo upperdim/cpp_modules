d5aac8f5f480d3c45ceb4a9cc543c47929428a2e
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-29 07:19:27 +0200

    added more tests

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 06:15:11 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/29 06:17:21 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/29 07:17:19 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,73 @@
 #include "Cure.hpp"
 #include "Ice.hpp"
 
+static void printHeader(std::string header) {
+	std::cout                                                   << std::endl;
+	std::cout << "============================================" << std::endl;
+	std::cout << header                                         << std::endl;
+	std::cout << "============================================" << std::endl;
+}
+
 int main() {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-	
-	ICharacter* me = new Character("me");
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-	
-	ICharacter* bob = new Character("bob");
-	
-	me->use(0, *bob);
-	me->use(1, *bob);
-	
-	delete bob;
-	delete me;
-	delete src;
+	printHeader("Subject PDF tests");
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		
+		ICharacter* me = new Character("me");
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		
+		ICharacter* bob = new Character("bob");
+		
+		me->use(0, *bob);
+		me->use(1, *bob);
+		
+		delete bob;
+		delete me;
+		delete src;
+	}
+
+	printHeader("MateriaSource inventory full");
+	{
+		IMateriaSource* src = new MateriaSource();
+		
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		src->learnMateria(new Ice()); // inventory full
+		
+		delete src;
+	}
+
+	printHeader("Use unquipped materia");
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		
+		ICharacter* me = new Character("me");
+		ICharacter* bob = new Character("bob");
+
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		me->use(1, *bob);
+		me->unequip(1);
+		me->use(1, *bob);
+
+		delete src;
+		delete me;
+		delete bob;
+		delete tmp;
+	}
 	
 	return 0;
 }
