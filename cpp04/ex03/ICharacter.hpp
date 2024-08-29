6dfbe76d58e9ddee5a63264b9a69a26229a7bd86
upperdim <45048352+upperdim@users.noreply.github.com>
2024-08-29 03:53:33 +0200

    added includes

--- cpp04/ex03/ICharacter.hpp
+++ cpp04/ex03/ICharacter.hpp
@@ -6,12 +6,15 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 03:16:05 by tunsal            #+#    #+#             */
-/*   Updated: 2024/08/29 03:52:21 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/08/29 03:53:22 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ICHARACTER_H
 #define ICHARACTER_H
+
+#include <string>
+#include "AMateria.hpp"
 
 class ICharacter {
 public:
