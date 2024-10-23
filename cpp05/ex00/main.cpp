1e49348c8924752417ab0327b8cebdeec9e73db8
upperdim <45048352+upperdim@users.noreply.github.com>
2024-10-23 19:01:57 +0200

    was calling another variable with the same name. `high` and `low` in try block is not visible form catch block

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 20:43:04 by tunsal            #+#    #+#             */
-/*   Updated: 2024/10/23 17:22:27 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/10/23 18:56:09 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,20 @@ int main() {
 		std::cout << std::endl;
 	}
 
+	std::string attemptedName = "Attempted Name";
 	try {
-		Bureaucrat high = Bureaucrat("High", GRADE_HIGHEST - 1);
+		Bureaucrat highAttempt = Bureaucrat(attemptedName, GRADE_HIGHEST - 1);
 	} catch (std::exception & e) {
-		std::cout << "Catched exception of following bureaucrat:" << std::endl;
-		std::cout << high << std::endl << "Exception = " << e.what() << std::endl;
+		std::cout << "Catched exception while creating bureaucrat \"" << attemptedName << "\":" << std::endl;
+		std::cout << "Exception = " << e.what() << std::endl;
 		std::cout << std::endl;
 	}
 
 	try {
-		Bureaucrat low = Bureaucrat("Low", GRADE_LOWEST + 1);
+		Bureaucrat lowAttempt = Bureaucrat(attemptedName, GRADE_LOWEST + 1);
 	} catch (std::exception & e) {
-		std::cout << "Catched exception of following bureaucrat:" << std::endl;
-		std::cout << low << std::endl << "Exception = " << e.what() << std::endl;
+		std::cout << "Catched exception while creating bureaucrat \"" << attemptedName << "\":" << std::endl;
+		std::cout << "Exception = " << e.what() << std::endl;
 		std::cout << std::endl;
 	}
 }
