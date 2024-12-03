f12d150602e1494426d65680034f44dfd082cf88
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-03 19:36:05 +0100

    added test cases to main

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,41 +6,79 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 20:43:04 by tunsal            #+#    #+#             */
-/*   Updated: 2024/11/14 14:48:36 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/03 19:30:46 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Bureaucrat.hpp"
-#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+
+static void printHeader(std::string header) {
+	std::cout                                                   << std::endl;
+	std::cout << "============================================" << std::endl;
+	std::cout << header                                         << std::endl;
+	std::cout << "============================================" << std::endl;
+}
+
+void pardonFormTests() {
+	printHeader("Presidential Pardon Form tests");
+	
+	PresidentialPardonForm pardonForm = PresidentialPardonForm("pardon_target"); // exec:   5, sign: 25
+	Bureaucrat low = Bureaucrat("low", 30); // can't exec or sign
+	Bureaucrat mid = Bureaucrat("mid", 15); // can't exec, can sign
+	Bureaucrat high = Bureaucrat("high", 1); // can't exec, can sign
+
+	low.signForm(pardonForm);
+	low.executeForm(pardonForm);
+
+	mid.signForm(pardonForm);
+	mid.executeForm(pardonForm);
+
+	high.signForm(pardonForm);
+	high.executeForm(pardonForm);
+}
+
+void robotomyFormTests() {
+	printHeader("Robotomy Request Form tests");
+	
+	RobotomyRequestForm robotomyForm = RobotomyRequestForm("robotomy_target");   // exec:  45, sign: 72
+	Bureaucrat low = Bureaucrat("low", 75); // can't exec or sign
+	Bureaucrat mid = Bureaucrat("mid", 55); // can't exec, can sign
+	Bureaucrat high = Bureaucrat("high", 30); // can't exec, can sign
+
+	low.signForm(robotomyForm);
+	low.executeForm(robotomyForm);
+
+	mid.signForm(robotomyForm);
+	mid.executeForm(robotomyForm);
+
+	high.signForm(robotomyForm);
+	high.executeForm(robotomyForm);
+}
+
+void creationFormTests() {
+	printHeader("Shrubbery Creation Form tests");
+	
+	ShrubberyCreationForm creationForm = ShrubberyCreationForm("creationForm");  // exec: 137, sign: 145
+	Bureaucrat low = Bureaucrat("low", 147); // can't exec or sign
+	Bureaucrat mid = Bureaucrat("mid", 139); // can't exec, can sign
+	Bureaucrat high = Bureaucrat("high", 130); // can't exec, can sign
+
+	low.signForm(creationForm);
+	low.executeForm(creationForm);
+
+	mid.signForm(creationForm);
+	mid.executeForm(creationForm);
+
+	high.signForm(creationForm);
+	high.executeForm(creationForm);
+}
 
 int main() {
-	// AForm aform = AForm("aform name", 100, 100); // Cannot instantiate abstract class
-	
-	// std::string attemptedName = "Attempted Name";
-	// try {
-	// 	Form low = Form(attemptedName, GRADE_LOWEST + 1, GRADE_LOWEST);
-	// } catch (std::exception & e) {
-	// 	std::cout << "Exception when creating " << attemptedName << ":" << std::endl;
-	// 	std::cout << e.what() << std::endl << std::endl;
-	// }
-
-	// try {
-	// 	Form low = Form(attemptedName, GRADE_LOWEST, GRADE_LOWEST + 1);
-	// } catch (std::exception & e) {
-	// 	std::cout << "Exception when creating " << attemptedName << ":" << std::endl;
-	// 	std::cout << e.what() << std::endl << std::endl;
-	// }
-
-	// Bureaucrat lowBureaucrat = Bureaucrat("Low Bureaucrat", GRADE_LOWEST);
-	// Form midForm = Form("Mid Form", (GRADE_LOWEST - GRADE_HIGHEST) / 2, (GRADE_LOWEST - GRADE_HIGHEST) / 2);
-	// Bureaucrat highBureaucrat = Bureaucrat("High Bureaucrat", GRADE_HIGHEST);
-
-	// std::cout << midForm << std::endl;
-	// lowBureaucrat.signForm(midForm);
-	// std::cout << midForm << std::endl;
-	// highBureaucrat.signForm(midForm);
-	// std::cout << midForm << std::endl;
-	// highBureaucrat.signForm(midForm);
-	
+	pardonFormTests();
+	robotomyFormTests();
+	pardonFormTests();
 }
