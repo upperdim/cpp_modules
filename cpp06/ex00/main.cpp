0cc44611084f5a982853669f6f4dcf502b9f4930
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-07 18:20:06 +0100

    main program

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,10 +6,19 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:35:18 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/05 16:35:18 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/07 18:12:52 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main() {
+#include <iostream>
+#include "ScalarConverter.hpp"
 
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		std::cout << "Usage: ./convert <number literal>" << std::endl;
+		return 0;
+	}
+
+	ScalarConverter::convert(argv[1]);
+	return 0;
 }
