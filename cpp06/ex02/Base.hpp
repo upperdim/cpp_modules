fe1781af52ec8e93a6df14252fe64da1197ecd3c
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-08 20:29:45 +0100

    implement base

--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -6,7 +6,16 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 19:50:48 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/08 19:50:49 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/08 20:18:48 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base {
+	public:
+		virtual ~Base();
+};
+
+#endif
