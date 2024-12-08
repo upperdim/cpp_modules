17f47e7c4f5ea74134d7d8980fbc9c8378591706
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-08 20:30:01 +0100

    template of main

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -6,7 +6,24 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 19:51:07 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/08 19:51:08 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/08 20:27:48 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// `typeinfo` header is FORBIDDEN
+#include "Base.hpp"
+
+// Randomly instanciate A, B or C, return a Base pointer to the instance
+Base * generate(void) {
+
+}
+
+// Print what type `p` is
+void identify(Base* p) {
+
+}
+
+// Print what type `p` is WITHOUT USING A POINTER
+void identify(Base& p) {
+	
+}
