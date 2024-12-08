6ea83c0539db9c0ae72d8222de5a04684b257f21
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-08 22:22:22 +0100

    cpp07 ex00 solution

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,38 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 22:02:57 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/08 22:02:58 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/08 22:06:58 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+#include <iostream>
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return 0;
+}
+
+// Should output:
+// a = 3, b = 2
+// min(a, b) = 2
+// max(a, b) = 3
+// c = chaine2, d = chaine1
+// min(c, d) = chaine1
+// max(c, d) = chaine2
+// 6
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,7 +6,28 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 22:02:58 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/08 22:02:59 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/08 22:20:43 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T& a, T& b) {
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b) {
+	return a < b ? a : b;
+}
+
+template <typename T>
+T max(T a, T b) {
+	return a > b ? a : b;
+}
+
+#endif
