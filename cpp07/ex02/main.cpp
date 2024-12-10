fce35477f421c9a6da50c11f8685e95ec8755b3d
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-10 02:31:45 +0100

    stopped using `this`

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,12 +6,17 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 23:50:11 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/09 23:50:26 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/10 02:21:41 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Array.hpp"
 
 int main() {
-
+	// Tip: Try to compile int * a = new int(); then display *a.
+	int *a = new int();
+	std::cout << a << std::endl;
+	
+	
 }
