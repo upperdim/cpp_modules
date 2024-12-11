c73b58ca0526103fb2afe333b812f3c0366b8110
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-11 08:39:31 +0100

    header orthodox canonical form

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,14 +6,22 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 08:24:01 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/11 08:24:45 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/11 08:30:32 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-
+template <typename T>
+class MutantStack {
+private:
+	MutantStack();
+	~MutantStack();
+	MutantStack(MutantStack const & from);
+	MutantStack& operator=(MutantStack const & rhs);
+public:
+};
 
 #include "MutantStack.tpp"
 
