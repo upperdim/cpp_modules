74c87a800d2d368295e1b62c22d3bfec0a544f5a
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-11 08:39:37 +0100

    subject tests

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,12 +6,81 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 08:24:00 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/11 08:24:56 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/11 08:39:10 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostring>
 #include "MutantStack.hpp"
 
-int main() {
+void subjectTestWithList() {
+	std::cout << "-------------------------------------------" << std::endl;
+	std::cout << " Running Subject Test (Replaced with list) " << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+	
+	std::list<int> mstack;
 
+	mstack.push_back(5);
+	mstack.push_back(17);
+	
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+	
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	//[...]
+	mstack.push_back(0);
+	
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+	
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+}
+
+void subjectTest() {
+	std::cout << "-------------------------------------------" << std::endl;
+	std::cout << " Running Subject Test                      " << std::endl;
+	std::cout << "-------------------------------------------" << std::endl;
+
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+	
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << mstack.size() << std::endl;
+	
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+}
+
+int main() {
+	
 }
