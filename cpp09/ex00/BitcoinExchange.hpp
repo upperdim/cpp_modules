6b96a0c9df77151e594681aeeab5411d75c89fe3
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-17 10:50:22 +0100

    make methods private

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 00:36:24 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/17 10:33:28 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/17 10:47:46 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ class BitcoinExchange {
 private:
 	std::map<std::string, float>	_exchangeRates;
 
+	void 			printDb();
+	void			printMap(std::map<std::string, float> m);
 public:
 	BitcoinExchange();
 	BitcoinExchange(BitcoinExchange const & other);
@@ -27,8 +29,6 @@ public:
 	~BitcoinExchange();
 	
 	void			readDb(const std::string dbFilename);
-	void 			printDb();
-	void			printMap(std::map<std::string, float> m);
 	bool			validateDateStr(std::string date);
 	float			getBitcoinValue(std::string date, float amount);
 	std::string		getEarliestDate();
