85d073e7cbce3f99b51e15bf8cdf2bd370880ce9
upperdim <45048352+upperdim@users.noreply.github.com>
2024-12-19 06:45:19 +0100

    deque tests

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tunsal <tunsal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 23:27:04 by tunsal            #+#    #+#             */
-/*   Updated: 2024/12/19 05:46:50 by tunsal           ###   ########.fr       */
+/*   Updated: 2024/12/19 06:43:28 by tunsal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 #include <algorithm>
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <deque>
 #include <ctime>
 #include "PmergeMe.hpp"
 
+static void printHeader(std::string header) {
+	std::cout                                                   << std::endl;
+	std::cout << "============================================" << std::endl;
+	std::cout << header                                         << std::endl;
+	std::cout << "============================================" << std::endl;
+}
+
+// -----------------------------------------------------------------------------
+// Vector
+// -----------------------------------------------------------------------------
 bool areVectorsSame(const std::vector<int>& vec1, const std::vector<int>& vec2) {
 	if (vec1.size() != vec2.size()) {
 		return false;
@@ -31,7 +43,9 @@ bool areVectorsSame(const std::vector<int>& vec1, const std::vector<int>& vec2) 
 	return true;
 }
 
-void automaticTests(PmergeMe sorter, size_t repeatCount, size_t minVal, size_t maxVal, size_t numCount, bool prints) {
+void automaticTestsVec(PmergeMe sorter, size_t repeatCount, size_t minVal, size_t maxVal, size_t numCount, bool prints) {
+	printHeader("Running Automatic Tests (Vector)");
+	
 	if (minVal > maxVal || minVal == 0 || maxVal == 0) {
 		std::cout << "Invalid arguments in automatic tests!" << std::endl;
 		return;
@@ -59,7 +73,9 @@ void automaticTests(PmergeMe sorter, size_t repeatCount, size_t minVal, size_t m
 	}
 }
 
-void manualTests(PmergeMe sorter) {
+void manualTestsVec(PmergeMe sorter) {
+	printHeader("Running Manual Tests (Vector)");
+
 	std::vector<std::vector<int>> tests = {
 		{6, 7, 8, 5, 12, 9, 3, 1, 2, 4, 11},
 		{1, 9, 2, 8, 3, 7, 4, 6, 5, 10, 20, 11, 19, 12, 18, 13, 17, 14, 16, 15, 10},
@@ -75,6 +91,74 @@ void manualTests(PmergeMe sorter) {
 	}
 }
 
+// -----------------------------------------------------------------------------
+// Deque
+// -----------------------------------------------------------------------------
+bool areDequesSame(const std::deque<int>& vec1, const std::deque<int>& vec2) {
+	if (vec1.size() != vec2.size()) {
+		return false;
+	}
+
+	for (size_t i = 0; i < vec1.size(); ++i) {
+		if (vec1[i] != vec2[i]) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void automaticTestsDeque(PmergeMe sorter, size_t repeatCount, size_t minVal, size_t maxVal, size_t numCount, bool prints) {
+	printHeader("Running Automatic Tests (Deque)");
+
+	if (minVal > maxVal || minVal == 0 || maxVal == 0) {
+		std::cout << "Invalid arguments in automatic tests!" << std::endl;
+		return;
+	}
+
+	for (size_t i = 0; i < repeatCount; ++i) {
+		std::cout << i << " - Sorting " << numCount << " random numbers between " << minVal << " and " << maxVal << "...";
+
+		std::deque<int> randomList;
+		for (size_t j = 0; j < numCount; ++j) {
+			int randomNum = minVal + std::rand() % (maxVal - minVal + 1);
+			randomList.push_back(randomNum);
+		}
+
+		std::deque<int> actual = sorter.sort(randomList, prints);
+
+		std::deque<int> expected = randomList;
+		std::sort(expected.begin(), expected.end());
+
+		if (areDequesSame(actual, expected)) {
+			std::cout << "OK" << std::endl;
+		} else {
+			std::cout << "FAIL!" << std::endl;
+		}
+	}
+}
+
+void manualTestsDeque(PmergeMe sorter) {
+	printHeader("Running Manual Tests (Deque)");
+
+	std::deque<std::deque<int>> tests = {
+		{6, 7, 8, 5, 12, 9, 3, 1, 2, 4, 11},
+		{1, 9, 2, 8, 3, 7, 4, 6, 5, 10, 20, 11, 19, 12, 18, 13, 17, 14, 16, 15, 10},
+		{2, 2, 2, 2, 6, 6, 6, 6, 4}
+	};
+
+	for (size_t t = 0; t < tests.size(); ++t) {
+		std::cout << "\n\n\n\nList = ";
+		sorter.printDeque("", tests[t]);
+
+		std::deque<int> sorted = sorter.sort(tests[t], true);
+		sorter.printDeque("Sorted = ", sorted);
+	}
+}
+
+// -----------------------------------------------------------------------------
+// Main
+// -----------------------------------------------------------------------------
 long long getMicroseconds() {
 	struct timeval time;
 	gettimeofday(&time, NULL);
@@ -82,6 +166,21 @@ long long getMicroseconds() {
 }
 
 int main(int argc, char *argv[]) {
+	PmergeMe sorter;
+	std::srand(std::time(0)); // seed rand() for tests, once in the program
+	
+	// Tests (Vector)
+	// manualTestsVec(sorter);
+	// automaticTestsVec(sorter, 25, 1, 4000000, 50000, false);
+	// automaticTestsVec(sorter, 1, 1, 40, 15, true);
+	// return 0;
+
+	// Tests (Deque)
+	// manualTestsDeque(sorter);
+	// automaticTestsDeque(sorter, 25, 1, 4000000, 50000, false);
+	// automaticTestsDeque(sorter, 1, 1, 40, 15, true);
+	// return 0;
+	
 	if (argc < 2) {
 		std::cout << "Usage: " << argv[0] << " <positive int 1> <positive int 2> <positive int 3> <...>" << std::endl;
 		return 0;
@@ -108,24 +207,24 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 	
-	PmergeMe sorter;
-	
-	// manualTests(sorter);
-	// std::srand(std::time(0)); // seed rand(), once in the program
-	// automaticTests(sorter, 3, 1, 4000000, 50000, false);
-	// automaticTests(sorter, 3, 1, 40, 15, true);
-	// return 0;
-	
+	// Vector
 	long long startTime = getMicroseconds();
-	std::vector<int> beforeVecSorted = sorter.sort(beforeVec, false);
+	std::vector<int> afterVec = sorter.sort(beforeVec, false);
 	long long endTime = getMicroseconds();
-
 	long long vectorSortTime = endTime - startTime;
 	
+	// Deque
+	std::deque<int> beforeDeque(beforeVec.begin(), beforeVec.end());
+	startTime = getMicroseconds();
+	std::deque<int> afterDeque = sorter.sort(beforeDeque, false);
+	endTime = getMicroseconds();
+	long long dequeSortTime = endTime - startTime;
+
+	// Print
 	sorter.printList("Before:\t", beforeVec);
-	sorter.printList("After:\t", beforeVecSorted);
-	std::cout << std::fixed << "Time to process a range of " << beforeVec.size() << " elements with std::vector : " << vectorSortTime << " us" << std::endl;
-	// Display time taken by second container	Time to process a range of 5 elements with std::[..] : 0.00014 us
+	sorter.printList("After:\t", afterVec);
+	std::cout << std::fixed << "Time to process a range of " << beforeVec.size()   << " elements with std::vector : " << vectorSortTime << " us" << std::endl;
+	std::cout << std::fixed << "Time to process a range of " << beforeDeque.size() << " elements with std::deque  : "  << dequeSortTime  << " us" << std::endl;
 	
 	return 0;
 }
